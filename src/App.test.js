b6d481211df7b6c42a3e7f95fd734728ac9003e1
Vipul Jindal <vipul.jindal@publicissapient.com>
2020-03-18 13:55:30 +0530

    added

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 import { shallow } from "enzyme";
 import Postform from "./components/testingComponent"
 
